74aedc5252d3c5ae1e4bba0c50c54083f5e3114c
sasha0277 <83279321+sasha0277@users.noreply.github.com>
2022-03-10 22:13:55 +0100

    Create-React-App

---
+++ amazoneClone/frontend/src/App.js
@@ -0,0 +1,12 @@
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <a href="http://">AmazonClone</a>
+      </header>
+      <main>Listings Products</main>
+    </div>
+  );
+}
+
+export default App;
