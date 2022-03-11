6dec77203f2e6e1b5e92346950990378930e455d
sasha0277 <83279321+sasha0277@users.noreply.github.com>
2022-03-11 17:20:21 +0100

    React Bootstarp

--- amazoneClone/frontend/src/App.js
+++ amazoneClone/frontend/src/App.js
@@ -1,19 +1,35 @@
-import { BrowserRouter, Link, Route, Routes } from 'react-router-dom';
+import Container from 'react-bootstrap/Container';
+import Navbar from 'react-bootstrap/Navbar';
+import { LinkContainer } from 'react-router-bootstrap';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import HomeScreen from './screens/HomeScreen';
 import ProductScreen from './screens/ProductScreen';
+
 function App() {
   return (
     <BrowserRouter>
-      <div className="App">
-        <header className="App-header">
-          <Link to="/">AmazonClone</Link>
+      <div className="d-flex flex-column site-container">
+        <header>
+          <Navbar bg="dark" variant="dark">
+            <Container>
+              <LinkContainer to="/">
+                <Navbar.Brand>Amazon Clone</Navbar.Brand>
+              </LinkContainer>
+            </Container>
+          </Navbar>
         </header>
         <main>
-          <Routes>
-            <Route path="/product/:slug" element={<ProductScreen />} />
-            <Route path="/" element={<HomeScreen />} />
-          </Routes>
+          <Container>
+            <Routes>
+              <Route path="/product/:slug" element={<ProductScreen />} />
+              <Route path="/" element={<HomeScreen />} />
+            </Routes>
+          </Container>
         </main>
+
+        <footer>
+          <div className="text-center">All Right Reserved To Sonny Sharma</div>
+        </footer>
       </div>
     </BrowserRouter>
   );
